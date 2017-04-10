c74d1e5d9e8c498f33eb3af9d15bbf3fd78f7b7f
Bash <bashash10@gmail.com>
2017-04-10 10:38:36 +0100

    Files uploaded
    Ver 0.0.1 of the webapp UI and is still in development, but is almost ready for React js and any other js libraries

---
+++ jq-test.js
@@ -0,0 +1,69 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    $('#settings').click(function(){
+      $('.bkg-menu').removeClass("hide");
+    });
+
+    $('#profile').click(function(){
+      $('.bkg-menu').removeClass("hide");
+    });
+
+    $('#menu-close').click(function(){
+      $('.bkg-menu').addClass("hide");
+    });
+
+    $(function() {
+
+        $('#settings').click(function() {
+
+            if ($('#set-title').hasClass("hide")) {
+                $('#set-title').removeClass('hide');
+            }
+            else if (!$('#set-title').hasClass("hide")) {
+                $('#set-title').addClass('hide');
+            }
+
+        });
+
+    });
+
+    $(function() {
+
+        $('#profile').click(function() {
+
+            if ($('#pro-title').hasClass("hide")) {
+                $('#pro-title').removeClass('hide');
+            }
+            else if (!$('#pro-title').hasClass("hide")) {
+                $('#pro-title').addClass('hide');
+            }
+
+        });
+
+    });
+
+    $(function() {
+
+        $('#menu-close').click(function() {
+
+            if (!$('#set-title').hasClass("hide")) {
+                $('#set-title').addClass('hide');
+            }
+
+        });
+
+    });
+
+    $(function() {
+
+        $('#menu-close').click(function() {
+
+            if (!$('#pro-title').hasClass("hide")) {
+                $('#pro-title').addClass('hide');
+            }
+
+        });
+
+    });
+});
